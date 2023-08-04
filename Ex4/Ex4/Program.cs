dade191ed3a66f6cb21e8acb3d403c8d55e3cdce
Dudu <137932110+EduardoFrancisc@users.noreply.github.com>
2023-08-04 14:22:18 -0300

    ex5

--- Ex4/Ex4/Program.cs
+++ Ex4/Ex4/Program.cs
@@ -3,17 +3,14 @@
     //Faça um programa que calcule a gorjeta a ser paga de uma conta de restaurante. A gorjeta é calculada como sendo 10% do valor da conta, que deve ser informado pelo usuário.
     public static void Main(string[] args)
     {
-        double totalConta, gorjeta=0;
+        double totalConta, gorjeta = 0;
 
-        while (true)
-        {
-            Console.WriteLine("Qual foi o valor total da conta?");
-            double.TryParse(Console.ReadLine(), out totalConta);
+        Console.WriteLine("Qual foi o valor total da conta?");
+        double.TryParse(Console.ReadLine(), out totalConta);
 
-            gorjeta = totalConta * 0.10;
+        gorjeta = totalConta * 0.10;
 
-            Console.WriteLine("O valor da gorjeta é " + gorjeta);
-            Console.ReadKey();
-        }
+        Console.WriteLine("O valor da gorjeta é " + gorjeta);
+        Console.ReadKey();
     }
 }
---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    //Faça um programa que calcule o novo valor de um salário a partir de um valor percentual de reajuste. O valor atual do salário e o valor percentual do reajuste devem ser informados pelo usuário como, por exemplo, 7,3%.
+    public static void Main(string[] args)
+    {
+        float salario, percentual, novoSalario, ajuste;
+        String controle;
+
+        Console.WriteLine("Qual o atual valor do salário?");
+        float.TryParse(Console.ReadLine(), out salario);
+
+
+        Console.WriteLine("Qual o percentual do ajuste para o salário?");
+        float.TryParse(Console.ReadLine(), out percentual);
+
+        Console.WriteLine("Acréscimo(+) ou Decréscimo(-)?");
+        controle = Console.ReadLine();
+
+        ajuste = (percentual / 100) * salario;
+
+        switch (controle)
+        {
+            case "+":
+                novoSalario = salario + ajuste;
+                Console.WriteLine("O novo salário é: " + novoSalario);
+                break;
+            case "-":
+                novoSalario = salario - ajuste;
+                Console.WriteLine("O novo salário é: " + novoSalario);
+                break;
+            default:
+                Console.WriteLine("Entrada inválida");
+                 return;
+        }
+        Console.ReadKey();
+    }
+
+}
