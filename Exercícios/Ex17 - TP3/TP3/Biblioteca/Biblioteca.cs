09267a3f9c52cdc66495fa50a6e170096c90e61f
Dudu <137932110+EduardoFrancisc@users.noreply.github.com>
2023-09-28 17:13:32 -0300

    Entrega do tp3

---
+++ Exercícios/Ex17 - TP3/TP3/Biblioteca/Biblioteca.cs
@@ -0,0 +1,76 @@
+﻿namespace Biblioteca
+{
+    public interface ICadastrarPessoa
+    {
+        void AdicionarPessoa(Pessoa pessoa);
+        List<Pessoa> ListaPessoa();
+    }
+    public class Pessoa
+    {
+        private string _nome { get; set; }
+        private string _sobrenome { get; set; }
+        private int _idade { get; set; }
+        private double _altura { get; set; }
+        private DateTime _dataNascimento { get; set; }
+        private bool _deficiente { get; set; }
+
+        public Pessoa(string nome, string sobrenome, double altura, bool deficiente, DateTime dataNascimento)
+        {
+            _nome = nome + " " + sobrenome;
+            _altura = altura;
+            _dataNascimento = dataNascimento;
+            _deficiente = deficiente;
+        }
+
+        public Pessoa()
+        {
+        }
+
+        public DateTime DataNascimento
+        {
+            get { return _dataNascimento; }
+        }
+        public double Altura
+        {
+            get { return _altura; }
+        }
+        public int Idade
+        {
+            get { return CalculaIdade(_dataNascimento); }
+        }
+        public string Nome
+        {
+            get { return _nome; }
+        }
+        public bool Deficiente
+        {
+            get { return _deficiente; }
+        }
+
+        //Método que calcula a idade
+        public static int CalculaIdade(DateTime dataNascimto)
+        {
+            int age = 0;
+
+            return DateTime.Now.Year - dataNascimto.Year;
+        }
+    }
+    public class CadastraPessoaEmMemoria : ICadastrarPessoa
+    {
+        private List<Pessoa> _listPessoa;
+        public CadastraPessoaEmMemoria()
+        {
+            _listPessoa = new List<Pessoa>();
+        }
+
+        public void AdicionarPessoa(Pessoa pessoa)
+        {
+            _listPessoa.Add(pessoa);
+        }
+
+        public List<Pessoa> ListaPessoa()
+        {
+            return _listPessoa;
+        }
+    }
+}
---
+++ Exercícios/Ex17 - TP3/TP3/TP3/Program.cs
@@ -0,0 +1,135 @@
+﻿using Biblioteca;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Program {
+    public static void MenuPadrao()
+    {
+        Console.Clear();
+        Console.WriteLine(" Selecione uma das opções abaixo:");
+        Console.WriteLine("    1 - Pesquisar Pessoa      ");
+        Console.WriteLine("    2 - Adicionar nova Pessoa ");
+        Console.WriteLine("    3 - Sair                  ");
+    }
+
+    public static void MenuInclusao(ICadastrarPessoa i)
+    {
+        double altura;
+        bool deficiencia;
+        DateTime dataNascimento;
+
+        Console.Clear();
+        Console.WriteLine("Qual o Nome da Pessoa que deseja adicionar?");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Qual o Sobrenome da Pessoa que deseja adicionar?");
+        string sobrenome = Console.ReadLine();
+        Console.WriteLine("Qual a altura da Pessoa que deseja adicionar?");
+        double.TryParse(Console.ReadLine(), out altura);
+        Console.WriteLine("Qual a data de Nascimento no formato [dd/mm/yyyy]");
+        DateTime.TryParse(Console.ReadLine(), out dataNascimento);
+        Console.WriteLine("A Pessoa que deseja adicionar pussui alguma Deficiência? [true | false]");
+        bool.TryParse(Console.ReadLine(), out deficiencia);
+
+        Console.Clear();
+        Console.WriteLine("OS DADOS ABAIXO ESTÃO CORRETOS?");
+        Console.WriteLine("Nome: " + nome + " " + sobrenome);
+        Console.WriteLine("Idade: " + Pessoa.CalculaIdade(dataNascimento));
+        Console.WriteLine("Altura: " + altura);
+        Console.WriteLine("Possui deficiência: " + deficiencia);
+        Console.WriteLine("Data de Nascimento: " + dataNascimento.ToString("dd/MM/yyyy"));
+        string confirmacaoAdicao = Console.ReadLine();
+        confirmacaoAdicao.ToLower();
+
+
+        if (confirmacaoAdicao == "sim")
+        {
+            Console.WriteLine("Pessoa Adicionada com Sucesso!!!");
+            Pessoa pessoa = new(nome, sobrenome, altura, deficiencia, dataNascimento);
+            i.AdicionarPessoa(pessoa);
+            Console.ReadKey();
+            Console.Clear();
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Pessoa não adicionada");
+            Console.ReadKey();
+            Console.Clear();
+            return;
+        }
+    }
+
+    public static void MenuConsulta(ICadastrarPessoa i)
+    {
+
+        string pesquisa = string.Empty;
+        while (pesquisa != "x" && pesquisa != "X")
+        {
+            Console.WriteLine("Qual nome deseja pesqusiar?");
+            pesquisa = Console.ReadLine();
+
+            Pessoa auxPessoa = i.ListaPessoa().Find(s => s.Nome.Contains(pesquisa));
+
+            if (auxPessoa != null)
+            {
+                Console.WriteLine($"{auxPessoa.Nome} se encontra na lista");
+                Console.WriteLine();
+                Console.WriteLine("Nome: " + auxPessoa.Nome);
+                Console.WriteLine("Idade: " + auxPessoa.Idade);
+                Console.WriteLine("Altura: " + auxPessoa.Altura);
+                Console.WriteLine("Possui deficiência: " + auxPessoa.Deficiente);
+                Console.WriteLine("Data de Nascimento: " + auxPessoa.DataNascimento.ToString("dd/MM/yyyy"));
+
+                Console.ReadKey();
+                break;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Entrada inválida");
+                Console.ReadKey();
+                break;
+            }
+        }
+
+    }
+
+    public static void Main(string[] args)
+    {
+        ICadastrarPessoa i = new CadastraPessoaEmMemoria();
+        /*instanciando entidades*/
+
+        try
+        {
+            while (true)
+            {
+                MenuPadrao();
+
+                int decisaoMenu;
+                int.TryParse(Console.ReadLine(), out decisaoMenu);
+
+                if (decisaoMenu == 1) //Pesquisar Pessoa
+                {
+                    MenuConsulta(i);
+
+                }
+                else if (decisaoMenu == 2) //Adicionar nova Pessoa
+                {
+                    MenuInclusao(i);
+
+                }
+                else if (decisaoMenu == 3) //Sair 
+                {
+                    Console.WriteLine("Obrigado por usar esta Aplicação");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada Inválida");
+                }
+            }
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }}
