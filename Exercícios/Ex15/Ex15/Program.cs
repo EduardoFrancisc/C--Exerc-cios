330b197be1ba33d129fbb0f2919193493e990ade
Eduardo Francisco <duudu.0304@gmail.com>
2024-02-21 19:13:28 -0300

    nomeandoExercícios

--- Exercícios/Ex15/Ex15/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿class Program
-{
-    //Faça um programa que leia uma opção de um menu sendo[1] Soma, [2] Subtração, [3] Multiplicação e[4] Divisão.Se a opção for válida, o programa deverá ler os operandos, realizar a operação e mostrar o resultado.Caso contrário, o programa deverá exibir uma mensagem de operação inválida.
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
